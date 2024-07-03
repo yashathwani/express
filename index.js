08f5508db3027c700da8dcb44969aa87eb79ab74
Yash Athwani <yashathwani45@gmail.com>
2024-07-03 10:19:45 +0530

    receiving the post request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port=3000;
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
@@ -14,4 +15,8 @@ let courses=[
 
 app.get('/courses', (req, res) => {
     res.json(courses);
+});
+app.post('/courses', (req, res) => {
+    courses.push({id:courses.length+1, name:req.body.name});
+    console.log(req.body);
 });
